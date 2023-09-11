a39ba41aee1ed6751a6322e4ab6265af01200de2
Suti1999 <ozsvartbence11@gmail.com>
2023-09-11 09:05:05 +0200

    first commit

---
+++ elso.js
@@ -0,0 +1,28 @@
+const a_input = document.getElementById("a_szam");
+const b_input = document.getElementById("b_szam");
+const kijelzo=document.getElementById("eredmeny");
+
+function osszead(){
+    a=parseFloat(a_input.value);
+    b=parseFloat(b_input.value);
+    kijelzo.innerHTML="Összeadva" + (a+b).toString();
+
+}
+function kivon(){
+    a=parseFloat(a_input.value);
+    b=parseFloat(b_input.value);
+    kijelzo.innerHTML="Kivonva" + (a-b).toString();
+
+}
+function szorzas(){
+    a=parseFloat(a_input.value);
+    b=parseFloat(b_input.value);
+    kijelzo.innerHTML="Összeszorozva" + (a*b).toString();
+
+}
+function osztva(){
+    a=parseFloat(a_input.value);
+    b=parseFloat(b_input.value);
+    kijelzo.innerHTML="Osztva" + (a/b).toString();
+
+}
